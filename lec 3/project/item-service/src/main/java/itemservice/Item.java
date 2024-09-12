12afeaaf4ba98c55dcf8c6dea6bf2409535b9337
Eslam.khder <eslamkhder11@gmail.com>
2024-09-12 22:23:53 +0300

    lec3

---
+++ lec 3/project/item-service/src/main/java/itemservice/Item.java
@@ -0,0 +1,46 @@
+package itemservice;
+
+public class Item {
+
+	private int id;
+	
+	private String name;
+	
+	private int price;
+	
+	private int totalNumber;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getTotalNumber() {
+		return totalNumber;
+	}
+
+	public void setTotalNumber(int totalNumber) {
+		this.totalNumber = totalNumber;
+	}
+	
+	
+}
