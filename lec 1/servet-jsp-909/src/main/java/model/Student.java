d8a42a9840b97aec5a3c8e035bbbb3815dc7ce5d
Eslam.khder <eslamkhder11@gmail.com>
2025-12-21 22:08:28 +0200

    909

---
+++ lec 1/servet-jsp-909/src/main/java/model/Student.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Student {
+
+	private Long id;
+	
+	private String name;
+	
+	private String phone;
+
+	public Student(Long id, String name, String phone) {
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	
+}
